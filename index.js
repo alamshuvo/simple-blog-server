f3a77ef6c476be5997c516235490daa024540b4d
iftakhar <iftakharalamshuvo11@gmail.com>
2024-05-12 11:04:08 +0600

    post and get route added

--- index.js
+++ index.js
@@ -26,7 +26,30 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
+  const blogCollection= client.db('simpleDB').collection('blogDB')
+
+
+
+
+// add blog on db
+  app.post("/blog",async(req,res)=>{
+    const newBlog=req.body;
+    console.log(newBlog);
+    const result=await blogCollection.insertOne(newBlog)
+    res.send(result)
+  })
+
   
+  // all blog get
+  app.get("/blog",async(req,res)=>{
+    const cursor=blogCollection.find();
+    const result = await cursor.toArray();
+    res.send(result);
+    })
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
