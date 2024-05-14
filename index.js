5b369fc51cba3324d1a24aaa4995640aeaef8577
iftakhar <iftakharalamshuvo11@gmail.com>
2024-05-14 13:32:29 +0600

    featured blog section filter route added

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cokieeParser=require('cookie-parser')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
+const jwt =require('jsonwebtoken')
 require('dotenv').config()
 const app =express();
 const port =process.env.PORT || 5000;
 
 // middlewere
 
-app.use(cors());
+app.use(cors({
+  origin:[
+    'http://localhost:5173',
+  
+  ],
+  credentials:true
+}));
 app.use(express.json());
+app.use(cokieeParser());
+
 
 
 
@@ -23,6 +34,32 @@ const client = new MongoClient(uri, {
   }
 });
 
+// middleware
+const logger=(req,res,next)=>{
+  // console.log('loginfo', req.method,req.url);
+  next()
+}
+
+const varifiyToken=(req,res,next)=>{
+  const token=req?.cookies?.token;
+  // console.log("cookies in the middleare",token);
+  // no token availabail
+  if (!token) {
+    return res.status(401).send({message:'unautharazied access'})
+  }
+  jwt.verify(token,process.env.ACCESS_TOKEN,(err,decoded)=>{
+    if (err) {
+      return res.status(401).send({massage:'unautharized access'})
+    }
+  req.user=decoded;
+  next();
+  })
+  // next()
+}
+
+
+
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -30,9 +67,34 @@ async function run() {
   const commentCollection= client.db('simplecommentDB').collection('commentDB')
   const wishlistCollection= client.db('simplewishlistDB').collection('wishlistDB')
 
+console.log(process.env.ACCESS_TOKEN);
+
+app.post("/jwt" ,async(req,res)=>{
+const user =req.body;
+const token=jwt.sign(user,process.env.ACCESS_TOKEN,{expiresIn:'1h'})
+console.log("user",user);
+res.cookie('token',token,{
+  httpOnly:true,
+  secure:true,
+  sameSite:'none'
+})
+.send({sucess:true})
+})
+
+
+app.post("/logout",async(req,res)=>{
+  const  user=req.body;
+  console.log(user);
+  res.clearCookie('token',{maxAge:0}).send({sucess:true})
+})
 
 
 
+
+
+
+
+// services related api
 // add blog on db
   app.post("/blog",async(req,res)=>{
     const newBlog=req.body;
@@ -131,11 +193,18 @@ app.get("/wishlist", async(req,res)=>{
     const result = await cursor.toArray();
     res.send(result);
     })
-
+  app.get("/featured", async(req,res)=>{
+    const cursor=blogCollection.find().sort({"long": 1});
+    const result = await cursor.toArray();
+    res.send(result);
+    })
+ 
  
 // single blog item
     app.get("/blog/id/:id",async(req,res)=>{
         const id =req.params.id;
+        // console.log("token owner info",req.user);
+       
         const quary={_id:new ObjectId(id)};
         const result=await blogCollection.findOne(quary);
         res.send(result)
